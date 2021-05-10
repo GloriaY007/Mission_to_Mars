4713a4a0bdffc8a51857ed28d01e3c7f98856c31
GloriaY007 <glorrossy@gmail.com>
2021-05-10 13:56:36 -0400

    Running Flask application

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import Mission_to_Mars
 
 #Set Up Flask
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = Mission_to_Mars.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
